cb731328deef34e9056b28b542d4780f5e415e3d
ga02chu <152811379+ga02chu@users.noreply.github.com>
2026-04-22 09:17:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -85,4 +85,4 @@ def index():
     return "Bot is running!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
